8acb7dc3779b8e614646d7cc41c15eacf18667e9
s1_1v <s1_1v@naver.com>
2025-11-16 22:09:52 +0900

    Feat: VNode 생성 함수 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,15 @@
+// 자식 평탄화 + null, undefined, false 제거
+const normalizeChildren = (children) =>
+  children
+    .flat(Infinity)
+    .filter(
+      (child) => child !== false && child !== null && child !== undefined,
+    );
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type: type,
+    props: props,
+    children: normalizeChildren(children),
+  };
 }
