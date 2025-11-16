319641170fb1869a33d43da07644227fc31d6cef
s1_1v <s1_1v@naver.com>
2025-11-16 23:24:29 +0900

    Feat: normalizeVNode 형변환 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,11 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
   return vNode;
 }
