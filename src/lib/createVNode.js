7977936101e556fa8bb79b6cdbf298a323209595
minjeeki <minjeeki@student.42seoul.kr>
2025-11-18 01:32:41 +0900

    fix: JSX 표현 결과에 대한 필터링 코드 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,7 +1,16 @@
 export function createVNode(type, props, ...children) {
+  const flattened = children.flat(Infinity);
+  const filtered = flattened.filter(
+    (child) =>
+      child !== null &&
+      child !== undefined &&
+      child !== false &&
+      child !== true,
+  );
+
   return {
     type: type,
     props: props,
-    children: children.flat(Infinity),
+    children: filtered,
   };
 }
