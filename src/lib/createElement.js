9e4751d47110f73ffbf1204bbc482eec57101645
김민석 <61860897+kju1018@users.noreply.github.com>
2025-11-19 01:15:14 +0900

    feat: createElement, createVNode, normalizeVNode 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,26 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode.toString());
+  }
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      fragment.appendChild(createElement(child));
+    });
+    return fragment;
+  }
+
+  const $el = document.createElement(vNode.type);
+  vNode.children.forEach((child) => {
+    $el.appendChild(createElement(child));
+  });
+  return $el;
+}
 
 function updateAttributes($el, props) {}
