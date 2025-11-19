0c5b53a343a2c0b9ea6f519ddb8e16cecf6bf32c
김민석 <61860897+kju1018@users.noreply.github.com>
2025-11-20 01:15:41 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -7,32 +7,37 @@ export function normalizeVNode(vNode) {
     return vNode.toString();
   }
 
-  // 함수형 컴포넌트인 경우 실행
-  if (typeof vNode.type === "function") {
-    const props = vNode.props || {};
-    // children을 props에 포함시켜야 함
-    const componentProps = {
-      ...props,
-      children: vNode.children || [],
+  // vNode가 객체인 경우 (가상 DOM 노드)
+  if (vNode && typeof vNode === "object" && vNode.type) {
+    // 함수형 컴포넌트인 경우 실행
+    if (typeof vNode.type === "function") {
+      const props = vNode.props || {};
+      // children을 props에 포함시켜야 함
+      const componentProps = {
+        ...props,
+        children: vNode.children || [],
+      };
+      const componentResult = vNode.type(componentProps);
+      return normalizeVNode(componentResult);
+    }
+
+    // 일반 엘리먼트인 경우 자식들을 재귀적으로 정규화
+    const normalizedChildren = (vNode.children || [])
+      .map((child) => normalizeVNode(child))
+      .filter(
+        (child) =>
+          child !== null &&
+          child !== undefined &&
+          child !== "" &&
+          typeof child !== "boolean",
+      );
+
+    return {
+      type: vNode.type,
+      props: vNode.props,
+      children: normalizedChildren,
     };
-    const componentResult = vNode.type(componentProps);
-    return normalizeVNode(componentResult);
   }
 
-  // 일반 엘리먼트인 경우 자식들을 재귀적으로 정규화
-  const normalizedChildren = (vNode.children || [])
-    .map((child) => normalizeVNode(child))
-    .filter(
-      (child) =>
-        child !== null &&
-        child !== undefined &&
-        child !== "" &&
-        typeof child !== "boolean"
-    );
-
-  return {
-    type: vNode.type,
-    props: vNode.props,
-    children: normalizedChildren,
-  };
+  return vNode;
 }
