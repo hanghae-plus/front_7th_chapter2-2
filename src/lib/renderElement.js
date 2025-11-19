1f389b509c7bc5fa85fe026827cb8ad9bda75584
hanseul jung <hanseul56524@gmail.com>
2025-11-20 01:22:14 +0900

    feat: updateElement 함수 추가 - renderElement 최초 렌더링 이후 분기 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,26 +1,43 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-//import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
-  // let isInitialRender = true;
-  // const el = normalizeVNode(vNode);
-  // const newEl = createElement(el);
-  // container.appendChild(newEl);
+  const el = normalizeVNode(vNode);
 
-  // if (isInitialRender) {
-  //   setupEventListeners(container);
-  //   isInitialRender = false;
-  // } else {
-  //   return;
-  // }
-  const normalized = normalizeVNode(vNode);
-  const newTree = createElement(normalized);
+  if (!container._vNode) {
+    // 최초 렌더
+    const newEl = createElement(el);
+    container.replaceChildren(newEl);
+  } else {
+    // 이후 렌더
+    const nextChildren = Array.isArray(el) ? el : [el];
+    // 이전 노드 상태를 _vNode 속성에 저장
+    const prevChildren = Array.isArray(container._vNode)
+      ? container._vNode
+      : container._vNode
+        ? [container._vNode]
+        : [];
 
-  container.replaceChildren(newTree);
+    const sharedLength = Math.min(nextChildren.length, prevChildren.length);
+    for (let i = 0; i < sharedLength; i += 1) {
+      updateElement(container, nextChildren[i], prevChildren[i], i);
+    }
+
+    if (nextChildren.length > prevChildren.length) {
+      for (let i = sharedLength; i < nextChildren.length; i += 1) {
+        updateElement(container, nextChildren[i], undefined, i);
+      }
+    } else if (prevChildren.length > nextChildren.length) {
+      for (let i = prevChildren.length - 1; i >= sharedLength; i -= 1) {
+        updateElement(container, undefined, prevChildren[i], i);
+      }
+    }
+  }
+  container._vNode = el;
   setupEventListeners(container);
 }
