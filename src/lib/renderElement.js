fa962ad82769baecd496e3b4c019f8350056776c
윤지 <85012406+yoonhihi97@users.noreply.github.com>
2025-11-20 19:03:46 +0900

    refactor: 디버깅용 console.log 제거

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -4,8 +4,6 @@ import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  console.log("[renderElement] 렌더링 시작");
-
   // normalizeVNode를 해서
   // 함수 컴포넌트 → 실제 HTML 요소로 변환
   // null/undefined → ""로 변환
@@ -14,13 +12,11 @@ export function renderElement(vNode, container) {
 
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   if (!oldVNode) {
-    console.log("[renderElement] 최초 렌더링");
     // 최초 렌더링
     container.appendChild(createElement(normalizedVNode));
     // 최초 렌더링 시에만 이벤트 등록!
     setupEventListeners(container);
   } else {
-    console.log("[renderElement] 재렌더링");
     // 재렌더링
     updateElement(container, normalizedVNode, oldVNode);
     // 재렌더링 시에는 이벤트 등록 안 함!
@@ -28,6 +24,4 @@ export function renderElement(vNode, container) {
 
   // 다음 렌더링 때 비교 하기 위해 저장
   container._vNode = normalizedVNode;
-
-  console.log("[renderElement] 렌더링 완료");
 }
