5a6b8b7ad45f3192c07011c15863b526bfe123a2
jthw1005 <hjt105@gmail.com>
2025-11-20 21:13:44 +0900

    feat: renderElement

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,10 +1,21 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-import { updateElement } from "./updateElement";
+// import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  // 1. vNode를 정규화
+  const normalized = normalizeVNode(vNode);
+
+  // 2. container를 비우고 새로운 DOM 생성
+  container.innerHTML = "";
+
+  // 3. createElement로 노드를 만들고
+  const element = createElement(normalized);
+
+  // 4. container에 삽입
+  container.appendChild(element);
+
+  // 5. 이벤트를 등록
+  setupEventListeners(container);
 }
