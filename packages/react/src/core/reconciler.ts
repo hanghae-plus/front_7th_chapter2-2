1e46927b1c85a4fa4a5732eeaca3fb59bc1341e3
Yongtae Park <samuel.park@mz.co.kr>
2025-11-21 03:47:57 +0900

    feat(reconcile): implement reconcile logic

--- packages/react/src/core/reconciler.ts
+++ packages/react/src/core/reconciler.ts
@@ -1,5 +1,5 @@
 import { context } from "./context";
-import { Fragment, NodeTypes, TEXT_ELEMENT } from "./constants";
+import { Fragment, NodeTypes, TEXT_ELEMENT, NodeType } from "./constants";
 import { Instance, VNode } from "./types";
 import {
   getFirstDom,
@@ -8,6 +8,7 @@ import {
   removeInstance,
   setDomProps,
   updateDomProps,
+  createInstance,
 } from "./dom";
 import { createChildPath } from "./elements";
 import { isEmptyValue } from "../utils";
@@ -29,10 +30,49 @@ export const reconcile = (
 ): Instance | null => {
   // 여기를 구현하세요.
   // 1. 새 노드가 null이면 기존 인스턴스를 제거합니다. (unmount)
+  if (node === null) {
+    removeInstance(parentDom, instance);
+    return null;
+  }
   // 2. 기존 인스턴스가 없으면 새 노드를 마운트합니다. (mount)
+  if (instance === null) {
+    return createInstance(node);
+  }
   // 3. 타입이나 키가 다르면 기존 인스턴스를 제거하고 새로 마운트합니다.
+  if (instance.node.type !== node.type || instance.key !== node.key) {
+    instance.node = node;
+    removeInstance(parentDom, instance);
+    return createInstance(node);
+  }
   // 4. 타입과 키가 같으면 인스턴스를 업데이트합니다. (update)
   //    - DOM 요소: updateDomProps로 속성 업데이트 후 자식 재조정
   //    - 컴포넌트: 컴포넌트 함수 재실행 후 자식 재조정
+  if (instance.node.type === node.type && instance.key === node.key) {
+    const prevProps = instance.node.props;
+    instance.node = node;
+
+    if (instance.kind === NodeTypes.TEXT) {
+      const textNode = instance.dom as Text;
+      const newNodeValue = (node.props as { nodeValue: string }).nodeValue;
+      if (textNode.nodeValue !== newNodeValue) {
+        textNode.nodeValue = newNodeValue;
+      }
+    } else if (instance.kind === NodeTypes.HOST && instance.dom) {
+      updateDomProps(instance.dom as HTMLElement, prevProps, node.props);
+    }
+
+    if (instance.kind === NodeTypes.COMPONENT || instance.kind === NodeTypes.FRAGMENT) {
+      instance.children =
+        node.props.children?.map((child, index) =>
+          reconcile(parentDom as HTMLElement, instance.children[index], child, ""),
+        ) ?? [];
+    } else if (instance.kind === NodeTypes.HOST) {
+      instance.children =
+        node.props.children?.map((child, index) =>
+          reconcile(instance.dom as HTMLElement, instance.children[index], child, ""),
+        ) ?? [];
+    }
+    return instance;
+  }
   return null;
 };
