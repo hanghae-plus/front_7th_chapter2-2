4b33af66730a031b7abe54df3d9adb717f9441ae
Yongtae Park <samuel.park@mz.co.kr>
2025-11-21 19:58:06 +0900

    feat: create hook manager

---
+++ packages/react/src/core/hookManager.ts
@@ -0,0 +1,28 @@
+import { context } from "./context";
+
+export const hookManager = {
+  runComponent<P extends Record<string, unknown> = Record<string, unknown>>(
+    path: string,
+    componentFunction: React.ComponentType<P>,
+    props: P,
+  ) {
+    context.hooks.componentStack.push(path);
+    context.hooks.cursor.set(path, 0);
+
+    try {
+      return componentFunction(props);
+    } finally {
+      context.hooks.componentStack.pop();
+    }
+  },
+
+  increaseCursor(path: string) {
+    const currentCursor = context.hooks.cursor.get(path) ?? 0;
+    context.hooks.cursor.set(path, currentCursor + 1);
+  },
+
+  increaseEffectCursor(path: string) {
+    const currentEffectCursor = context.hooks.effectCursor.get(path) ?? 0;
+    context.hooks.effectCursor.set(path, currentEffectCursor + 1);
+  },
+};
